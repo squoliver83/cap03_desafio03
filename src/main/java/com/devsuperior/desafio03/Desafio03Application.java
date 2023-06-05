a2fab07e505885f3f6482a210d314078ff8ddac0
Samuel Quaresma <squoliver@gmail.com>
2023-06-05 18:23:21 -0300

    Code cleaning and imports fix

--- src/main/java/com/devsuperior/desafio03/Desafio03Application.java
+++ src/main/java/com/devsuperior/desafio03/Desafio03Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Desafio03Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Desafio03Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Desafio03Application.class, args);
+    }
 
 }
--- src/main/java/com/devsuperior/desafio03/entities/Client.java
+++ src/main/java/com/devsuperior/desafio03/entities/Client.java
@@ -1,6 +1,5 @@
 package com.devsuperior.desafio03.entities;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
